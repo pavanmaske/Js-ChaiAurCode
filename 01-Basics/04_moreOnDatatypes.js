e93bd8b63fb0c9fb016d16c14d77c03a95c49ea8
pavanmaske <pavanmaske63512gmail.com>
2024-01-03 22:20:53 +0530

    Primitive and Non-Primitive Datatypes

---
+++ 01-Basics/04_moreOnDatatypes.js
@@ -0,0 +1,24 @@
+// primitive datatypes- call by value
+//7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+/*refernce datatypes
+Arrays, Objects, Functions*/
+
+//Defining arrays
+const heros = ["pavan", "shaktiman"]
+
+//object
+let myObj = {
+    name: "pavan",
+    age: 22,
+}
+
+//function
+const myFunction = function () {
+    console.log("hello")
+}
+
+//Objects and browser elements kr liye to js mai mastery
+
+//typeof gives type of variable
+//typeof of null is object
